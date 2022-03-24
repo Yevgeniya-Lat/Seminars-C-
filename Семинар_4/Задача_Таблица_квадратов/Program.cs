784643a117284416d287b6a96df41d50a45fa8ba
Yevgeniya-Lat <e9618281262@gmail.com>
2022-03-24 20:33:49 +0300

    second commit

---
+++ Семинар_4/Задача_Таблица_квадратов/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+    int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string stringValue = Console.ReadLine();
+    int Value = int.Parse(stringValue);
+
+return Value;    
+}
+
+int N = Prompt("Введите число:  .");
+int i = 1;
+
+    Console.Write(i + " ");
+
+while (i < N)
+{
+    i++;
+    System.Console.Write(i*i + " ");
+}
+   
